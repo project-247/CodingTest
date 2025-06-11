6eac20847ee8ef57d9a7ba2a7eb2323e63c1f920
247 <96806304+project-247@users.noreply.github.com>
2025-06-11 12:09:28 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int A, B;
-    cin >> A >> B; 
-    if (A > B) {
-        cout << ">" << endl;
-    } else if (A < B) {
-        cout << "<" << endl;
-    } else {
-        cout << "==" << endl;
-    }
-    return 0;
+int main()
+{
+	int A, B;
+	cin >> A >> B;
+	if (A > B)
+	{
+		cout << ">" << endl;
+	}
+	else if (A < B)
+	{
+		cout << "<" << endl;
+	}
+	else
+	{
+		cout << "==" << endl;
+	}
+	return 0;
 }
