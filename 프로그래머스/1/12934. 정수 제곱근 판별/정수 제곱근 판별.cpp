abfbd80451492d350285ba2caf8286f88fe1637c
247 <96806304+project-247@users.noreply.github.com>
2025-06-25 15:12:48 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,13 @@
+#include <cmath>  
+
+using namespace std;
+
+long long solution(long long n) {
+    long long root = sqrt(n); 
+
+    if (root * root == n) {
+        return (root + 1) * (root + 1);
+    } else {
+        return -1;
+    }
+}
